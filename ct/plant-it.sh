5c3efaf2842a699a59f39882f8fb455f7b940251
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-11 11:21:05 +0100

    fix header

--- ct/plant-it.sh
+++ ct/plant-it.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
-# Source: https://www.debian.org/
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: MickLesk (CanbiZ)
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://plant-it.org/
 
 APP="Plant-it"
 var_tags="plants;garden"
