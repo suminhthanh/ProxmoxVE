63f400931b8a635dc01778626f8bfbb77f5e1c22
tsu <smthanh@gmail.com>
2025-03-11 23:42:45 +0700

    mod n8n

--- install/n8n-install.sh
+++ install/n8n-install.sh
@@ -21,36 +21,9 @@ $STD apt-get install -y ca-certificates
 $STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
-msg_info "Setting up Node.js Repository"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
-
-msg_info "Installing Node.js"
-$STD apt-get update
-$STD apt-get install -y nodejs
-msg_ok "Installed Node.js"
-
 msg_info "Installing n8n (Patience)"
-$STD npm install --global patch-package
-$STD npm install --global n8n
+curl -fsSL https://raw.githubusercontent.com/suminhthanh/n8n-hosting/main/docker-compose/withPostgresAndWorker/install.sh | bash
 msg_ok "Installed n8n"
-
-msg_info "Creating Service"
-cat <<EOF >/etc/systemd/system/n8n.service
-[Unit]
-Description=n8n
-
-[Service]
-Type=simple
-Environment="N8N_SECURE_COOKIE=false"
-ExecStart=n8n start
-[Install]
-WantedBy=multi-user.target
-EOF
-$STD systemctl enable --now n8n
-msg_ok "Created Service"
 
 motd_ssh
 customize
